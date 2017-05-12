ed1c47d0ee0bf42cc5a16473a2d71714ca5e2213
tiago_martins <v912166749@gmail.com>
2017-05-12 19:47:20 +0100

    home: solicitar impressao

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,13 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Home</div>
 
                 <div class="panel-body">
-
-                    {!! Form::open(['url' => 'foo/bar']) !!}
-
-
-
-                    {!! Form::close() !!}
-
-
                     <div class="form-group">
                         <div class="col-md-6 col-md-offset-4">
                             <button type="submit" class="btn btn-primary">
-                                <li><a href="{{ Route('impressoes') }}">solicitar impressão</a></li>
+                                <a href="{{ Route('impressoes')}}" style="color:white">solicitar impressão</a>
                             </button>
                         </div>
                     </div>
