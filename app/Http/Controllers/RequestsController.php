ba4d6ee7cc7d4dadb5d8edeb0e6e44bd95a4789c
tiago_martins <v912166749@gmail.com>
2017-05-18 21:31:28 +0100

    Revert "Revert "ajuda gil""
    This reverts commit 6cedf551ef4900815a148236cc7506403ead2ba6.

---
+++ app/Http/Controllers/RequestsController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRequest;
+use App\RequestPrint;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
+
+class RequestsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('requests.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRequest $request)
+    {
+        $requestPrint = new RequestPrint($request->all());
+
+        $requestPrint->status= 0;
+        $requestPrint->owner_id = Auth::id();
+
+        $file = $request->file('file');
+        $filename = $request->file->hashName();
+
+        $file->store('print-jobs/'.$requestPrint->owner_id.'/');
+
+        $requestPrint->file = $filename;
+
+        $requestPrint->save();
+
+        return redirect(URL::Route('impressoes.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
